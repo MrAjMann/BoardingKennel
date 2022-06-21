6252793dc63a9b5e18cf1b1040db3d7d309c47bc
MrAjMann <anthonyjmann87@gmail.com>
2022-06-21 11:44:34 +1000

    Adjusted desktop Landing page and changed the image

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -1,21 +1,24 @@
 import Image from "next/image";
-import dog from "./assets/dog.png";
+import dog from "./assets/doga.jpg";
 import styledHero from "./Hero.module.css";
 const HeroSection = () => {
   return (
-    <div className={styledHero.container}>
+    <section className={styledHero.container}>
       <div className={styledHero.innerWrapper}>
-        <div className={styledHero.imgSection}>
-          <Image src={dog} layout="fill" alt="Dog" />
-        </div>
-        <div>
+
+        <div className={styledHero.leftSection}>
           <h1 className={styledHero.title}>Accomodation for all your pets</h1>
           <div className={styledHero.btnWrapper}>
             <button>Book Now</button>
           </div>
         </div>
+        <div className={styledHero.rightSection}>
+        <div className={styledHero.imgSection}>
+          <Image src={dog} layout="responsive" alt="Dog" width='2400px'height='3400px' />
+        </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
