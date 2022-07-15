2bf72fd73ef305923ba4de285a3ff976aa28688f
MrAjMann <anthonyjmann87@gmail.com>
2022-07-15 20:58:57 +1000

    Login still not working

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,108 +1,88 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import { getToken } from "next-auth/jwt"
-import { trpc } from "../../../utils/trpc";
+
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "../../../server/db/client";
-import { getSession, signIn } from "next-auth/react";
-
 
 export const authOptions: NextAuthOptions = {
-    
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: {
-                    label: "email",
-                    type: "email",
-                    placeholder:"Enter Your name",
-                },
-                password: {
-                    label: "password",
-                    type: "password",
-                    
-                },
-                
-                
-                
-            },
-            
-            async authorize(credentials, _req) {
-                // console.log('req', _req)
-                // console.log('cred', credentials)
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				email: {
+					label: "email",
+					type: "email",
+					placeholder: "Enter Your name",
+				},
+				password: {
+					label: "password",
+					type: "password",
+				},
+			},
 
+			async authorize(credentials, _req) {
+				// console.log('req', _req)
+				// console.log('cred', credentials)
 
-                const user = await prisma?.user?.findFirst({
-                    where: {
-                        email:  credentials?.email,
-                        password:  credentials?.password,
-                    },
-                    
-                
-                    
-                })
-                
-                console.log('user', user);
-                if (user) {
-                    // Any object returned will be saved in `user` property of the JWT
-                    
-                    return user
-                } else {
-                    // If you return null then an error will be displayed advising the user to check their details.
-                    return null
-            
-                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                  }
-               
-                
-            },
-        })
-    ],
-    callbacks: {
-        async session({ session, token, user, ...rest }) {
-            console.log("in session callback,", {session, token, user, rest})
-            
-            if (token) {
-                session.id = token.id;
-            }
-            console.log('sessionID: ', session.id);
-            return session
-      
-          },
-          async jwt({ token, user, account, ...rest }) {
-            console.log("in jwt user =", {token, user, account, rest})
-            if (token.sub === user?.id) {
-                console.log('token = id');
-                
-            }
-            if (user) {
-              token.id = user.id;
-            }
-      
-            return token
-          },
+				const user = await prisma?.user?.findFirst({
+					where: {
+						email: credentials?.email,
+						password: credentials?.password,
+					},
+				});
 
-        async redirect() {
-            return ("/")
-        }
-        
-    },
-    events: {
-        async signIn(message) { " on successful sign in " },
-        async session(message) { ' session is active  '},
+				console.log("user", user);
+				if (user) {
+					return user;
+				} else {
+					return null;
+				}
+			},
+		}),
+	],
+	callbacks: {
+		async session({ session, token, user, ...rest }) {
+			console.log("in session callback,", { session, token, user, rest });
 
-    },
-    pages: {
-        signIn:'/login'
-    },
-    session: {
-        strategy: "jwt"
-    },
+			if (token) {
+				session.id = token.id;
+			}
+			console.log("sessionID: ", session.id);
+			return session;
+		},
+		async jwt({ token, user, account, ...rest }) {
+			console.log("in jwt user =", { token, user, account, rest });
+			if (token.sub === user?.id) {
+				console.log("token = id");
+			}
+			if (user) {
+				token.id = user.id;
+			}
 
-    secret: process.env.NEXTAUTH_SECRET,
+			return token;
+		},
+
+		async redirect() {
+			return "/";
+		},
+	},
+	events: {
+		async signIn(message) {
+			" on successful sign in ";
+		},
+		async session(message) {
+			" session is active  ";
+		},
+	},
+	pages: {
+		signIn: "/login",
+	},
+	session: {
+		strategy: "jwt",
+	},
+
+	secret: process.env.NEXTAUTH_SECRET,
 };
-
 
 export default NextAuth(authOptions);
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,110 +1,102 @@
-// import {useState} from 'react'
-import type { Session } from "next-auth"
-import { getCsrfToken } from "next-auth/react"
 import { useState } from "react";
 import styledLogin from "../styles/Login.module.css";
 import AuthButton from "../components/AuthComponents/Buttons/authButton";
 
-import {useSession, signIn, signOut } from "next-auth/react"
-
-
-
-
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Login() {
-  const { data: session,  status } = useSession({
-    required: false,
-    onUnauthenticated() {
-     console.log('unauthenticated user' );
-     
-      <div><p>Incorrect credentials try again</p></div>
-    },
-  })
-  
-  const [userEmail, setUserEmail] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
- 
+	const { data: session, status } = useSession({
+		required: false,
+		onUnauthenticated() {
+      
+      console.log("unauthenticated user")
+      return(
 
- console.log('user session firstname', session?.user.firstName);
- 
+        <div>
+				<p>Incorrect credentials try again</p>
+			</div>
+          )
+      
 
-// console.log('Email', userEmail);
-// console.log('Status', status);
+		},
+	});
 
+	const [userEmail, setUserEmail] = useState("");
+	const [password, setPassword] = useState("");
 
+	console.log("user session firstname", session?.user.firstName);
 
-
-  if (status === "authenticated" && session )  {
+	if (status === "authenticated" && session) {
+		return (
+			<div className={styledLogin.loginContainer}>
+				<h1 className={styledLogin.loginTitle}>Sign Up</h1>
+				<form className={styledLogin.loginForm}>
+					<p>Signed in as {` ${session?.user.email}`}</p>
+					<p>Signed in as {session?.user.firstName || "undefined"}</p>
+					<button onClick={() => signOut()}>Logout</button>
+				</form>
+			</div>
+		);
+	} else if (status === "loading") {
     return (
-      <div className={styledLogin.loginContainer}>
-      <h1 className={styledLogin.loginTitle}>Sign Up</h1>
-      <form className={styledLogin.loginForm} >
-        
-        <p  >Signed in as {` ${session?.user.email}`}</p> 
-        <p  >Signed in as {session?.user.firstName || 'undefined'}</p> 
-        <button  onClick={() => signOut()} >
-        Logout
-       </button>
-      </form>
-          
-    </div>
-  );
-}else if (status === "loading")   {
-  <div style={{display: 'flex', justifyContent:'center'}}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+			return <p style={{ fontSize: "32px", color: "000" }}>Loading....</p>
+		</div>
+      )
+	} else {
+		return (
+			<div className={styledLogin.loginContainer}>
+				<h1 className={styledLogin.loginTitle}>Sign Up</h1>
+				<form
+					className={styledLogin.loginForm}
+					method="post"
+					action="/api/auth/callback/credentials"
+				>
+					<label className={styledLogin.loginLabel} htmlFor="email">
+						Email Address
+					</label>
+					<p>{userEmail}</p>
+					<p>{password}</p>
+					<input
+						className={styledLogin.loginInput}
+						type="email"
+						id="email"
+						name="email"
+						placeholder="Email"
+						onChange={(e) => setUserEmail(e.target.value)}
+						value={userEmail}
+					/>
 
-  return <p style={{fontSize: '32px' , color:'000'}}>Loading....</p>
-  </div>
-}else {
-  
-  return (
-    <div className={styledLogin.loginContainer}>
-    <h1 className={styledLogin.loginTitle}>Sign Up</h1>
-    <form className={styledLogin.loginForm} method="post" action="/api/auth/callback/credentials">
-      <label className={styledLogin.loginLabel} htmlFor="email">
-        Email Address
-      </label >
-      <p  >{userEmail}</p> 
-      <p  >{password}</p> 
-      <input
-      
-        className={styledLogin.loginInput}
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Email"
-        onChange={(e) => setUserEmail(e.target.value)}
-        value={userEmail}
-        />
+					<label className={styledLogin.loginLabel} htmlFor="password">
+						Password
+					</label>
 
-      <label className={styledLogin.loginLabel} htmlFor="password">
-        Password
-      </label>
-      
-      <input
-        className={styledLogin.loginInput}
-        id="password"
-        name="password"
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-        aria-label="Password"
-        />
+					<input
+						className={styledLogin.loginInput}
+						id="password"
+						name="password"
+						type="password"
+						placeholder="Password"
+						onChange={(e) => setPassword(e.target.value)}
+						value={password}
+						aria-label="Password"
+					/>
 
-            <button  onClick={() => signIn("credentials", {redirect: true, email: `${userEmail}`, password: `${password}`}).then(console.log)} >
-              
-        Login
-       </button>
-       
-    {/* <AuthButton  /> */}
-    </form>
-  </div>
-);
+					<button
+						onClick={() =>
+							signIn("credentials", {
+								redirect: true,
+								email: `${userEmail}`,
+								password: `${password}`,
+							}).then(console.log)
+						}
+					>
+						Login
+					</button>
+
+					{/* <AuthButton  /> */}
+				</form>
+			</div>
+		);
+	}
 }
-
-}
-
-
-
-
-
